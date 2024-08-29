0a6ac6d41ea695b9fe6c86eeef8f48f3b07a0b41
Kertis Henderson <khenderson@ncwgcap.org>
2024-08-29 14:38:28 -0400

    Implemented default slot

--- src/TerminalTextBox.vue
+++ src/TerminalTextBox.vue
@@ -1,10 +1,39 @@
 <template>
-    Hello.
+    <div>
+        {{ MainText }}
+    </div>
 </template>
 
 <script>
-    export default {
+    export default
+    {
         name: "TerminalTextBox",
+
+        props: {
+            contentClass: String,
+            chromeClass: String,
+        },
+
+        data ()
+        {
+            return {
+                MainText: "",
+            };
+        },
+
+        mounted()
+        {
+            if (this.$slots.hasOwnProperty("default"))
+            {
+                if (this.$slots.default().length > 0)
+                {
+                    if (this.$slots.default()[0].hasOwnProperty("children") > 0)
+                    {
+                        this.MainText = this.$slots.default()[0].children;
+                    }
+                }
+            }
+        },
     }
 </script>
 
